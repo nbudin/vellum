22cc294d15bf773e54953503695e5eab059030da
Nat Budin <natbudin@gmail.com>
2007-08-27 20:39:22 +0000

    Fix up model syntax; tests now pass
    git-svn-id: http://svn.aegames.org/svn/vellum@6 8055c45c-1b18-41ec-a88d-65353b77510d

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
   belongs_to :template_schema
-  has_many :documents, :dependent => destroy
-  has_many :structures, :dependent => destroy
+  has_many :documents, :dependent => :destroy
+  has_many :structures, :dependent => :destroy
 end
--- app/models/relationship_type.rb
+++ app/models/relationship_type.rb
@@ -1,6 +1,6 @@
 class RelationshipType < ActiveRecord::Base
   belongs_to :template_schema
-  has_many :relationships, :dependent => destroy
+  has_many :relationships, :dependent => :destroy
   has_one :left_template, :class_name => "Template"
   has_one :right_template, :class_name => "Template"
 end
