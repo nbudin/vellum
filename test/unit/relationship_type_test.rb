fe6344bc07740dc029fd152aa908bfdc109ed3cb
Nat Budin <natbudin@gmail.com>
2007-08-28 21:59:50 +0000

    Many more tests and useful validations.
    git-svn-id: http://svn.aegames.org/svn/vellum@10 8055c45c-1b18-41ec-a88d-65353b77510d

--- test/unit/relationship_type_test.rb
+++ test/unit/relationship_type_test.rb
@@ -1,10 +1,13 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class RelationshipTypeTest < Test::Unit::TestCase
-  fixtures :relationship_types
+  fixtures :relationship_types, :templates
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_illegal_type
+    p1 = Structure.new(:template => templates(:person))
+    p2 = Structure.new(:template => templates(:person))
+
+    r = Relationship.new(:relationship_type => relationship_types(:manager), :left => p1, :right => p2)
+    assert !r.valid?
   end
 end
