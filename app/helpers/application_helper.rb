74a52407b4294aa0400217557d07da1bfd618221
Nat Budin <natbudin@gmail.com>
2008-03-26 15:03:42 +0000

    Cosmetic fixes.
    git-svn-id: http://svn.aegames.org/svn/vellum@28 8055c45c-1b18-41ec-a88d-65353b77510d

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,21 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def nav_link_to(label, destination)
+    link_to label, destination, {:class => request.path =~ /^#{destination}/ ? "selected" : "" }
+  end
+  
+  def page_title
+    ret = if controller.action_name == 'index'
+      "List"
+    else
+      controller.action_name.capitalize
+    end
+    ret += " "
+    if controller.action_name != 'index'
+      ret += controller.controller_name.humanize.singularize.downcase
+    else
+      ret += controller.controller_name.humanize.downcase
+    end
+    return ret
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed by hooking up '' 
   # -- just remember to delete public/index.html.
-  map.connect '', :controller => "projects"
+  map.root :controller => "projects"
 
   # Allow downloading Web Service WSDL as a file with an extension
   # instead of a file named 'wsdl'
