851951a0f8757a83c4ee454978a61d6b83b92852
Nat Budin <natbudin@gmail.com>
2008-04-03 21:55:14 +0000

    Allow simple structure editing
    git-svn-id: http://svn.aegames.org/svn/vellum@30 8055c45c-1b18-41ec-a88d-65353b77510d

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,8 @@ ActionController::Routing::Routes.draw do |map|
   # map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
   # This route can be invoked with purchase_url(:id => product.id)
 
+  AeUsers.map_openid(map)
+  
   # You can have the root of your site routed by hooking up '' 
   # -- just remember to delete public/index.html.
   map.root :controller => "projects"
