160bfcd175eba7ab55bea56ba68c65b75b8ae8fd
Nat Budin <natbudin@gmail.com>
2008-06-03 22:05:47 +0000

    Fix some test cases
    git-svn-id: http://svn.aegames.org/svn/vellum@37 8055c45c-1b18-41ec-a88d-65353b77510d

--- test/functional/projects_controller_test.rb
+++ test/functional/projects_controller_test.rb
@@ -5,12 +5,13 @@ require 'projects_controller'
 class ProjectsController; def rescue_action(e) raise e end; end
 
 class ProjectsControllerTest < Test::Unit::TestCase
-  fixtures :projects
+  fixtures :projects, :template_schemas
 
   def setup
     @controller = ProjectsController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
+    @request.session[:person] = Person.find(:first).id
   end
 
   def test_should_get_index
@@ -18,15 +19,10 @@ class ProjectsControllerTest < Test::Unit::TestCase
     assert_response :success
     assert assigns(:projects)
   end
-
-  def test_should_get_new
-    get :new
-    assert_response :success
-  end
   
   def test_should_create_project
     old_count = Project.count
-    post :create, :project => { }
+    post :create, { :project => { :template_schema_id => template_schemas(:one).id } }
     assert_equal old_count+1, Project.count
     
     assert_redirected_to project_path(assigns(:project))
@@ -44,12 +40,14 @@ class ProjectsControllerTest < Test::Unit::TestCase
   
   def test_should_update_project
     put :update, :id => 1, :project => { }
-    assert_redirected_to project_path(assigns(:project))
+    assert_response :success
   end
   
   def test_should_destroy_project
     old_count = Project.count
-    delete :destroy, :id => 1
+    proj = Project.find(:first)
+    proj.grant(Person.find(@request.session[:person]))
+    delete :destroy, { :id => proj.id }
     assert_equal old_count-1, Project.count
     
     assert_redirected_to projects_path
