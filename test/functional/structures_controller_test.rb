841a028e6ff0880a9e0aaf0b31e8031c95b5786e
Nat Budin <natbudin@gmail.com>
2009-04-14 21:44:18 +0000

    Some test suite fixes

--- test/functional/structures_controller_test.rb
+++ test/functional/structures_controller_test.rb
@@ -5,51 +5,48 @@ require 'structures_controller'
 class StructuresController; def rescue_action(e) raise e end; end
 
 class StructuresControllerTest < ActionController::TestCase
-  fixtures :structures
+  fixtures :structures, :projects
 
   def setup
     @controller = StructuresController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
+
+    @project = projects(:one)
   end
 
   def test_should_get_index
-    get :index
+    get :index, :project_id => @project.id
     assert_response :success
     assert assigns(:structures)
   end
 
   def test_should_get_new
-    get :new
+    get :new, :project_id => @project.id
     assert_response :success
   end
   
   def test_should_create_structure
     old_count = Structure.count
-    post :create, :structure => { }
+    post :create, :project_id => @project.id, :structure => { }
     assert_equal old_count+1, Structure.count
     
     assert_redirected_to structure_path(assigns(:structure))
   end
 
   def test_should_show_structure
-    get :show, :id => 1
+    get :show, :project_id => @project.id, :id => 1
     assert_response :success
   end
 
-  def test_should_get_edit
-    get :edit, :id => 1
-    assert_response :success
-  end
-  
   def test_should_update_structure
-    put :update, :id => 1, :structure => { }
+    put :update, :project_id => @project.id, :id => 1, :structure => { }
     assert_redirected_to structure_path(assigns(:structure))
   end
   
   def test_should_destroy_structure
     old_count = Structure.count
-    delete :destroy, :id => 1
+    delete :destroy, :project_id => @project.id, :id => 1
     assert_equal old_count-1, Structure.count
     
     assert_redirected_to structures_path
