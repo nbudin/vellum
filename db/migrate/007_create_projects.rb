f42d28ebf62559ba644740b2b323d2abf181f66b
Nat Budin <natbudin@gmail.com>
2009-04-27 19:01:25 +0000

    Fix earlier migration to reference docs instead of documents
    git-svn-id: http://svn.aegames.org/svn/vellum@60 8055c45c-1b18-41ec-a88d-65353b77510d

--- db/migrate/007_create_projects.rb
+++ db/migrate/007_create_projects.rb
@@ -4,14 +4,14 @@ class CreateProjects < ActiveRecord::Migration
       t.column :name, :string
       t.column :template_schema_id, :integer
     end
-    add_column "documents", "project_id", :integer
-    add_column "document_versions", "project_id", :integer
+    add_column "docs", "project_id", :integer
+    add_column "doc_versions", "project_id", :integer
     add_column "structures", "project_id", :integer
   end
 
   def self.down
-    remove_column "documents", "project_id"
-    remove_column "document_versions", "project_id"
+    remove_column "docs", "project_id"
+    remove_column "doc_versions", "project_id"
     remove_column "structures", "project_id"
     drop_table :projects
   end
