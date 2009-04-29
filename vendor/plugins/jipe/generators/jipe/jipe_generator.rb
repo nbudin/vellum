56fb9eca4a22d179a5c5fbf9bfd9187de85e096c
Nat Budin <natbudin@gmail.com>
2009-04-29 17:43:16 -0400

    Fixing submodules

--- vendor/plugins/jipe/generators/jipe/jipe_generator.rb
+++
@@ -1,8 +0,0 @@
-class JipeGenerator < Rails::Generator::Base
-  def manifest
-    record do |m|
-      m.directory "public/images/jipe"
-      m.file 'edit-field.png', 'public/images/jipe/edit-field.png'
-    end
-  end
-end
