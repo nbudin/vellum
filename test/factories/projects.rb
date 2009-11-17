7ab0a13074d3446d40a5c4ff0c93f41cf9558def
Nat Budin <natbudin@gmail.com>
2009-11-17 10:33:49 -0500

    Factories

---
+++ test/factories/projects.rb
@@ -0,0 +1,18 @@
+Factory.define :project do |p|
+end
+
+Factory.define :structure do |s|
+end
+
+Factory.define :attr_value_metadata do |avm|
+end
+
+Factory.define :doc_value do |doc_value|
+end
+
+Factory.define :doc do |doc|
+end
+
+Factory.define :choice_field do |f|
+  
+end
