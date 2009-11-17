9ca1e70f75b8818b95b360008a4b178b22f33b3f
Nat Budin <natbudin@gmail.com>
2009-11-17 15:36:23 -0500

    MOAR TEST ACTION

--- test/unit/structure_test.rb
+++ test/unit/structure_test.rb
@@ -2,33 +2,100 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class StructureTest < ActiveSupport::TestCase
   fixtures :structures, :structure_templates, :text_fields, :relationship_types, :projects, :attrs
-
-  def test_create_structure
-    bob = Structure.new(:structure_template => structure_templates(:person), :project => projects(:people))
-    assert bob.save
-    assert name = bob.obtain_attr_value("Name")
-    name.value = "Bob"
-    bob.save
-
-    bob = Structure.find(bob.id)
-    assert bob.attr_value("Name").value == "Bob"
-  end
-
-  def test_related_structures
-    david = Structure.new(:structure_template => structure_templates(:worker), :project => projects(:people))
-    gareth = Structure.new(:structure_template => structure_templates(:worker), :project => projects(:people))
-    assert david.save
-    assert gareth.save
-
-    david_is_gareths_boss = Relationship.new(:left => david, :right => gareth, :relationship_type => relationship_types(:manager), :project => projects(:people))
-    assert david_is_gareths_boss.save, david_is_gareths_boss.errors.full_messages.join("\n")
-
-    david = Structure.find(david.id)
-    gareth = Structure.find(gareth.id)
-    assert david.relationships[0].relationship_type == relationship_types(:manager)
-    assert gareth.relationships[0].relationship_type == relationship_types(:manager)
-    assert david.relationships[0].other(david) == gareth
-    assert gareth.relationships[0].other(gareth) == david
-    assert david.relationships[0] = gareth.relationships[0]
+  
+  context "A new structure" do
+    setup do
+      @structure_template = Factory.create(:structure_template)
+      @project = Factory.create(:project, :template_schema => @structure_template.template_schema)
+      @bob = Factory.build(:structure, :structure_template => @structure_template, :project => @project)
+    end
+    
+    should "save cleanly" do
+      assert @bob.save
+    end
+    
+    context "with a name attr" do
+      setup do
+        assert @bob.save
+  
+        @attr_configuration = TextField.create
+        @attr = @bob.structure_template.attrs.create(:name => "Name", :attr_configuration => @attr_configuration)
+        @bob.reload
+      end
+      
+      should "create attr instances in memory on obtain_attr" do
+        assert attr = @bob.obtain_attr("Name")
+        assert @bob.attrs.include?(attr), "attr doesn't appear in @bob.attrs"
+        assert_equal attr, @bob.attr("Name")
+      end
+    
+      should "return the same attr on subsequent obtains" do
+        assert attr = @bob.obtain_attr("Name")
+        assert_equal attr, @bob.obtain_attr("Name")
+      end
+      
+      should "create AVM instances in memory on obtain_avm_for_attr" do
+        assert avm = @bob.obtain_avm_for_attr("Name")
+        assert @bob.attr_value_metadatas.include?(avm)
+      end
+      
+      should "return the same AVM instance on subsequent obtains" do
+        assert avm = @bob.obtain_avm_for_attr("Name")
+        assert_equal avm, @bob.obtain_avm_for_attr("Name")
+      end
+      
+      should "create attr value instances in memory on obtain_attr_value" do
+        assert av = @bob.obtain_attr_value("Name")
+        assert @bob.attr_values.include?(av), "av doesn't appear in @bob.attr_values"
+        assert_equal av, @bob.attr_value("Name")
+      end
+      
+      should "return the same attr_value instance on subsequent obtains" do
+        assert av = @bob.obtain_attr_value("Name")
+        av.value = "Bob"
+        assert av2 = @bob.obtain_attr_value("Name")
+        assert_equal av, av2
+        assert_equal av.value, av2.value
+      end
+      
+      context "having a value" do
+        setup do          
+          assert @attr_value = @bob.obtain_attr_value("Name")
+        end
+        
+        should "return the right value for name" do
+          @attr_value.value = "Bob"
+          assert_equal "Bob", @bob.name
+        end
+          
+        should "return the right value for attr_value.value" do
+          @attr_value.value = "Jim"
+          assert_equal "Jim", @bob.attr_value("Name").value
+        end
+      end
+    end
+    
+    context "with a related structure" do
+      setup do
+        assert @bob.save
+        
+        @rt = @structure_template.template_schema.relationship_types.create(:left_template => @structure_template,
+                                                                            :right_template => @structure_template)
+        
+        assert @joe = @project.structures.create(:structure_template => @structure_template)
+        assert @r = Factory.create(:relationship, :relationship_type => @rt, :left => @joe, :right => @bob, :project => @project)
+      end
+      
+      should "show the relationship on both structures" do
+        assert @r.save
+        @bob.reload
+        assert @bob.inward_relationships.include?(@r), "@r doesn't show up on Bob's inward relationships"
+        assert @bob.relationships.include?(@r), "@r doesn't show up on Bob's inward relationships"
+        
+        @joe.reload
+        assert @joe.outward_relationships.include?(@r), "@r doesn't show up on Joe's outward relationships"
+        assert @joe.relationships.include?(@r), "@r doesn't show up on Joe's inward relationships"
+      end
+    end
   end
 end
