2354ee2fb4d069221b59c6974ea157f138dcbb8e
Nat Budin <natbudin@gmail.com>
2009-11-23 14:35:03 -0500

    Switch to newer gemcutter versions of testing gems

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -18,5 +18,5 @@ config.action_controller.perform_caching             = false
 # ActionMailer::Base.deliveries array.
 config.action_mailer.delivery_method = :test
 
-config.gem 'thoughtbot-shoulda', :lib => "shoulda"
-config.gem 'thoughtbot-factory_girl', :lib => "factory_girl"
+config.gem 'shoulda', :source => "http://gemcutter.org"
+config.gem 'factory_girl', :source => "http://gemcutter.org", :version => "~> 1.2.3"
