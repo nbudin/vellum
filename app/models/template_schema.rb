abc5beddd26915496b896c8a2aebb719c1e00e71
Nat Budin <nbudin@kenichi-2.(none)>
2010-01-08 22:36:50 -0500

    Add publication templates

--- app/models/template_schema.rb
+++ app/models/template_schema.rb
@@ -1,6 +1,7 @@
 class TemplateSchema < ActiveRecord::Base
   has_many :projects
   has_many :structure_templates, :dependent => :destroy
+  has_many :publication_templates, :dependent => :destroy
   has_many :relationship_types, :dependent => :destroy
   
   acts_as_permissioned
