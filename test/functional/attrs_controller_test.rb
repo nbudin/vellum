1935ba0a0104a3119cf8165ef322d0a049f77041
Nat Budin <natbudin@gmail.com>
2010-01-20 17:08:11 -0500

    Complete rewrite of this test suite.  Now everything actually works!

--- test/functional/attrs_controller_test.rb
+++ test/functional/attrs_controller_test.rb
@@ -8,62 +8,127 @@ class AttrsControllerTest < ActionController::TestCase
   fixtures :attrs, :structure_templates, :template_schemas
 
   def setup
-    @controller = AttrsController.new
-    @request    = ActionController::TestRequest.new
-    @response   = ActionController::TestResponse.new
+    @person = Person.create
+    @request.session[:person] = @person.id
   end
 
-  def test_should_get_index
-    tmpl = structure_templates(:person)
-    get :index, :structure_template_id => tmpl.id, 
-      :template_schema_id => tmpl.template_schema.id
-    assert_response :success
-    assert assigns(:attrs)
+  context "on GET to :index" do
+    setup do
+      @tmpl = Factory.create(:structure_template)
+      @tmpl.template_schema.grant(@person)
+
+      get :index, :structure_template_id => @tmpl.id,
+        :template_schema_id => @tmpl.template_schema.id
+    end
+
+    should_assign_to :attrs
+    should_respond_with :success
   end
 
-  def test_should_get_new
-    tmpl = structure_templates(:person)
-    get :new, :structure_template_id => tmpl.id, 
-      :template_schema_id => tmpl.template_schema.id
-    assert_response :success
+  context "on GET to :new for an attr" do
+    setup do
+      @tmpl = Factory.create(:structure_template)
+      get :new, :structure_template_id => @tmpl.id,
+        :template_schema_id => @tmpl.template_schema.id
+    end
+
+    should_assign_to :structure_template
+    should_respond_with :success
+    should_render_template :new
+    should_not_set_the_flash
+  end
+
+  context "on POST to :create for an attr" do
+    setup do
+      @old_count = Attr.count
+      @tmpl = Factory.create(:structure_template)
+      post :create, :attr => { :name => "test" },
+        :structure_template_id => @tmpl.id,
+        :template_schema_id => @tmpl.template_schema.id,
+        :config_class => "TextField"
+    end
+    
+    should_assign_to :attr
+    should_respond_with :redirect
+    should_not_set_the_flash
+
+    should "create an attr" do
+      assert_equal @old_count + 1, Attr.count
+      assert_equal "test", assigns(:attr).name
+    end
+
+    should "redirect back to the template" do
+      assert_redirected_to structure_template_path(@tmpl.template_schema, @tmpl)
+    end
+  end
+
+  context "on GET to :show.json for an attr" do
+    setup do
+      @attr = Factory.create(:attr)
+      @attr.structure_template.template_schema.grant(@person)
+      get :show, :id => @attr.id,
+        :structure_template_id => @attr.structure_template.id,
+        :template_schema_id => @attr.structure_template.template_schema.id,
+        :format => 'json'
+    end
+
+    should_assign_to :attr
+    should_respond_with :success
+    should_not_set_the_flash
+
+    should "respond with valid JSON" do
+      @obj = begin
+        JSON.parse(@response.body)
+      rescue
+      end
+      assert @obj, "Response body (#{@response.body}) is not valid JSON"
+      assert_equal @attr.id, @obj["id"]
+    end
+  end
+
+  context "on PUT to :update for an attr" do
+    setup do
+      @attr = Factory.create(:attr)
+      @attr.structure_template.template_schema.grant(@person)
+    
+      put :update, :id => @attr.id, :attr => { :name => "SomethingElse" },
+        :template_schema_id => @attr.structure_template.template_schema.id,
+        :structure_template_id => @attr.structure_template.id,
+        :format => 'xml'
+    end
+
+    should_assign_to :attr
+    should_respond_with :success
+    should_not_set_the_flash
+
+    should "update the attr's fields" do
+      @attr.reload
+      assert_equal "SomethingElse", @attr.name
+    end
   end
   
-  def test_should_create_attr
-    old_count = Attr.count
-    tmpl = structure_templates(:person)
-    post :create, :attr => { :name => "test" }, :structure_template_id => tmpl.id, 
-      :template_schema_id => tmpl.template_schema.id, :config_class => "TextField"
-    assert_equal old_count+1, Attr.count
-    
-    assert_redirected_to structure_template_path(tmpl.template_schema, tmpl)
-  end
+  context "on DELETE to :destroy for an attr" do
+    setup do
+      @attr = Factory.create(:attr)
+      @attr.structure_template.template_schema.grant(@person)
 
-  def test_should_show_attr
-    attr = attrs(:name)
-    get :show, :id => attr.id, 
-      :template_schema_id => attr.structure_template.template_schema.id,
-      :structure_template_id => attr.structure_template.id
-    assert_response :success
-  end
+      @old_count = Attr.count
+      
+      delete :destroy, :id => @attr.id,
+        :structure_template_id => @attr.structure_template.id,
+        :template_schema_id => @attr.structure_template.template_schema.id
+    end
 
-  def test_should_update_attr
-    attr = attrs(:name)
-    put :update, :id => attr.id, :attr => { :name => "SomethingElse" },
-      :template_schema_id => attr.structure_template.template_schema.id,
-      :structure_template_id => attr.structure_template.id,
-      :format => :xml
-    assert_response :success
-  end
-  
-  def test_should_destroy_attr
-    old_count = Attr.count
-    attr = attrs(:name)
-    delete :destroy, :id => attr.id, 
-      :structure_template_id => attr.structure_template.id, 
-      :template_schema_id => attr.structure_template.template_schema.id
-    assert_equal old_count-1, Attr.count
-    
-    assert_redirected_to structure_template_path(attr.structure_template.template_schema,
-      attr.structure_template)
+    should_assign_to :attr
+    should_respond_with :redirect
+    should_not_set_the_flash
+
+    should "destroy an attr" do
+      assert_equal @old_count - 1, Attr.count
+    end
+
+    should "redirect back to the template" do
+      assert_redirected_to structure_template_path(@attr.structure_template.template_schema, @attr.structure_template)
+    end
   end
 end
