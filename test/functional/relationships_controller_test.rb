642bf3bd36bb664a2ee05a5cb755bede409de09c
Nat Budin <natbudin@gmail.com>
2010-01-21 10:49:05 -0500

    Rewrite relationships controller test suite

--- test/functional/relationships_controller_test.rb
+++ test/functional/relationships_controller_test.rb
@@ -1,45 +1,148 @@
 require 'test_helper'
 
 class RelationshipsControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:relationships)
+  def setup
+    create_logged_in_person
+
+    @rt = Factory.create(:relationship_type)
+    @schema = @rt.template_schema
+    @project = Factory.create(:project, :template_schema => @schema)
+    @project.grant(@person)
+    @left = Factory.create(:structure, :project => @project, :structure_template => @rt.left_template)
+    @right = Factory.create(:structure, :project => @project, :structure_template => @rt.right_template)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create relationship" do
-    assert_difference('Relationship.count') do
-      post :create, :relationship => { }
+  context "on GET to :index.json" do
+    setup do
+      get :index, :format => "json", :project_id => @project.id
     end
 
-    assert_redirected_to relationship_path(assigns(:relationship))
+    should_respond_with :success
+    should_assign_to :relationships
+    should_respond_with_json
   end
 
-  test "should show relationship" do
-    get :show, :id => relationships(:one).to_param
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, :id => relationships(:one).to_param
-    assert_response :success
-  end
-
-  test "should update relationship" do
-    put :update, :id => relationships(:one).to_param, :relationship => { }
-    assert_redirected_to relationship_path(assigns(:relationship))
-  end
-
-  test "should destroy relationship" do
-    assert_difference('Relationship.count', -1) do
-      delete :destroy, :id => relationships(:one).to_param
+  context "creating a new relationship" do
+    setup do
+      @referer = "http://xyzzy.com/plugh"
+      @request.env["HTTP_REFERER"] = @referer
+      @old_count = Relationship.count
     end
 
-    assert_redirected_to relationships_path
+    context "on POST to :create" do
+      setup do
+        post :create, :project_id => @project.id, :relationship => {
+          :relationship_type_id => @rt.id,
+          :left_id => @left.id,
+          :right_id => @right.id
+        }
+      end
+
+      should_respond_with :redirect
+      should_assign_to :relationship
+      should_not_set_the_flash
+
+      should "create a relationship" do
+        assert_equal @old_count + 1, Relationship.count
+      end
+
+      should "redirect back" do
+        assert_redirected_to @referer
+      end
+    end
+
+    context "and a new structure" do
+      setup do
+        @old_structure_count = Structure.count
+      end
+
+      context "on POST to :create with a new structure specified on the left" do
+        setup do
+          post :create, :project_id => @project.id, :relationship => {
+            :relationship_type_id => @rt.id,
+            :left_id => "new",
+            :right_id => @right.id
+          }
+        end
+
+        should_respond_with :redirect
+        should_assign_to :relationship
+        should_not_set_the_flash
+
+        should "create a relationship and a structure" do
+          assert_equal @old_count + 1, Relationship.count
+          assert_equal @old_structure_count + 1, Structure.count
+        end
+
+        should "redirect to the new structure" do
+          assert_redirected_to edit_structure_path(@project, assigns(:relationship).left)
+        end
+      end
+
+      context "on POST to :create with a new structure specified on the right" do
+        setup do
+          post :create, :project_id => @project.id, :relationship => {
+            :relationship_type_id => @rt.id,
+            :left_id => @left.id,
+            :right_id => "new"
+          }
+        end
+
+        should_respond_with :redirect
+        should_assign_to :relationship
+        should_not_set_the_flash
+
+        should "create a relationship and a structure" do
+          assert_equal @old_count + 1, Relationship.count
+          assert_equal @old_structure_count + 1, Structure.count
+        end
+
+        should "redirect to the new structure" do
+          assert_redirected_to edit_structure_path(@project, assigns(:relationship).right)
+        end
+      end
+    end
+  end
+
+  context "with a relationship" do
+    setup do
+      @relationship = Factory.create(:relationship, :project => @project, :relationship_type => @rt)
+    end
+
+    context "on PUT to :update.json" do
+      setup do
+        put :update, :id => @relationship.id, :project_id => @project.id,
+          :relationship => { :left_id => @left.id },
+          :format => "json"
+      end
+
+      should_respond_with :success
+      should_assign_to :relationship
+
+      should "update the relationship" do
+        assert_equal @left.id, assigns(:relationship).left_id
+      end
+    end
+
+    context "on DELETE to :destroy" do
+      setup do
+        @old_count = Relationship.count
+        @referer = "http://asdfljaqwioer"
+        @request.env["HTTP_REFERER"] = @referer
+        delete :destroy, :id => @relationship.id, :project_id => @project.id
+      end
+
+      should_respond_with :redirect
+      should_assign_to :relationship
+      should_not_set_the_flash
+
+      should "destroy a relationship" do
+        assert_equal @old_count - 1, Relationship.count
+      end
+
+      should "redirect to the referer" do
+        assert_redirected_to @referer
+      end
+    end
   end
 end
