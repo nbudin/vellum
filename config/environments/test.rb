5d3ad26e61c20c4ad4c8395d7f573ca08d8a2699
Nat Budin <natbudin@gmail.com>
2010-01-21 17:22:07 -0500

    Actually raise errors in test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -10,7 +10,7 @@ config.cache_classes = true
 config.whiny_nils = true
 
 # Show full error reports and disable caching
-config.action_controller.consider_all_requests_local = true
+config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = false
 
 # Tell ActionMailer not to deliver emails to the real world.
