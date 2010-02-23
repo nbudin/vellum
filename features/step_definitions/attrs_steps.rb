feca9045f53602f2853a19ec43ba6fd23791d507
Nat Budin <natbudin@gmail.com>
2010-02-23 11:31:06 -0500

    Prevent warning

--- features/step_definitions/attrs_steps.rb
+++ features/step_definitions/attrs_steps.rb
@@ -1,4 +1,4 @@
 Given /^a text field named "(.*)" on (.*)$/ do |name, template_name|
   assert tmpl = DocTemplate.find_by_name(template_name)
-  assert attr = tmpl.doc_template_attrs.create(:name => name, :ui_type => "text")
+  assert tmpl.doc_template_attrs.create(:name => name, :ui_type => "text")
 end
