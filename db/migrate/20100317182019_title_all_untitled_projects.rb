9ad0377616783a2cc77af6c64ac69b41362ccd3f
Nat Budin <natbudin@gmail.com>
2010-03-18 13:25:58 -0400

    Log what's going on

--- db/migrate/20100317182019_title_all_untitled_projects.rb
+++ db/migrate/20100317182019_title_all_untitled_projects.rb
@@ -2,12 +2,16 @@ class TitleAllUntitledProjects < ActiveRecord::Migration
   def self.up
     Project.all.each do |p|
       next unless p.name.blank?
+
+      say "Saving a name for project #{p.id}"
       p.name = "Untitled project"
       p.save!
     end
 
     DocTemplate.all.each do |t|
       next unless t.name.blank?
+
+      say "Saving a name for doc template #{t.id}"
       t.name = "Untitled template #{t.id}"
       t.save!
     end
