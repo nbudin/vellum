d7246bae0e6b2b38ea642431fcb8df90371a3bbb
Nat Budin <natbudin@gmail.com>
2011-02-22 17:46:25 -0500

    Add missing openid tables

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101014142211) do
+ActiveRecord::Schema.define(:version => 20110222201026) do
 
   create_table "attrs", :force => true do |t|
     t.integer "doc_version_id"
@@ -134,17 +134,18 @@ ActiveRecord::Schema.define(:version => 20101014142211) do
   end
 
   create_table "open_id_authentication_associations", :force => true do |t|
-    t.binary  "server_url"
-    t.string  "handle"
-    t.binary  "secret"
     t.integer "issued"
     t.integer "lifetime"
+    t.string  "handle"
     t.string  "assoc_type"
+    t.binary  "server_url"
+    t.binary  "secret"
   end
 
   create_table "open_id_authentication_nonces", :force => true do |t|
-    t.string  "nonce"
-    t.integer "created"
+    t.integer "timestamp",  :null => false
+    t.string  "server_url"
+    t.string  "salt",       :null => false
   end
 
   create_table "open_id_authentication_settings", :force => true do |t|
