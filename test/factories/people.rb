4084c6de29ea77be6a98cb7353f021f39d0ac04b
Nat Budin <natbudin@gmail.com>
2011-04-14 11:29:06 -0400

    Test case fixes for ability tests

--- test/factories/people.rb
+++ test/factories/people.rb
@@ -5,3 +5,8 @@ end
 Factory.define :person do |p|
   p.email { Factory.next(:email) }
 end
+
+Factory.define :project_membership do |m|
+  m.association :person
+  m.association :project
+end
--- test/unit/ability_test.rb
+++ test/unit/ability_test.rb
@@ -1,48 +1,112 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class AbilityTest < ActiveSupport::TestCase
-  class << self
-    def should_have_project_abilities(abilities)
-      %w{read create update destroy}.each do |action|
-        if abilities.include?(action)
-          should "be able to #{action} the project itself" do
-            assert @ability.can?(action, @project)
-          end
-        else
-          should "not be able to #{action} the project itself" do
-            assert !@ability.can?(action, @project)
-          end
-        end
+  def assert_has_project_abilities(abilities)
+    %w{read create update destroy}.each do |action|
+      if abilities.include?(action)
+        assert @ability.can?(action.to_sym, @project), "Should be able to #{action} the project"
+      else
+        assert !@ability.can?(action.to_sym, @project), "Should not be able to #{action} the project"
       end
     end
+  end
 
-    def should_have_content_abilities(abilities)
-      %w{read create update destroy}.each do |action|
-        [Doc, DocTemplate, Map, PublicationTemplate, RelationshipType, Relationship].each do |project_contents|
-          obj = project_contents.new(:project => @project)
-          if abilities.include?(action)
-            should "be able to #{action} a #{project_contents.name} in the project" do
-              assert @ability.can?(action, obj)
-            end
-          else
-            should "not be able to #{action} a #{project_contents.name} in the project" do
-              assert !@ability.can?(action, obj)
-            end
-          end
+  def assert_has_content_abilities(abilities)
+    %w{read create update destroy}.each do |action|
+      [Doc, DocTemplate, Map, PublicationTemplate, RelationshipType, Relationship].each do |klass|
+        obj = klass.new(:project => @project)
+        if abilities.include?(action)
+          assert @ability.can?(action.to_sym, obj), "Should be able to #{action} a #{klass.name} in the project"
+        else
+          assert !@ability.can?(action.to_sym, obj), "Should not be able to #{action} a #{klass.name} in the project"
         end
       end
     end
   end
   
-  context "An admin/author" do
+  def build_membership_and_ability(attributes={})
+    @membership = Factory.create(:project_membership, attributes)
+    @project = @membership.project
+    @person = @membership.person
+    @ability = Ability.new(@person)
+  end
+  
+  context "A project admin/author" do
+    setup { build_membership_and_ability(:admin => true, :author => true) }
+    
+    should "have appropriate permissions" do
+      assert_has_project_abilities(%w{read create update destroy})
+      assert_has_content_abilities(%w{read create update destroy})
+    end
+  end
+  
+  context "A project admin" do
+    setup { build_membership_and_ability(:admin => true, :author => false) }
+
+    
+    should "have appropriate permissions" do
+      assert_has_project_abilities(%w{read create destroy})
+      assert_has_content_abilities(%w{read})
+    end
+  end
+  
+  context "A project author" do
+    setup { build_membership_and_ability(:admin => false, :author => true) }
+
+    
+    should "have appropriate permissions" do
+      assert_has_project_abilities(%w{read create update})
+      assert_has_content_abilities(%w{read create update destroy})
+    end
+  end
+  
+  context "An unprivileged member" do
+    setup { build_membership_and_ability(:admin => false, :author => false) }
+    
+    should "have appropriate permissions" do
+      assert_has_project_abilities(%w{read create})
+      assert_has_content_abilities(%w{read})
+    end
+  end
+  
+  context "A non-member" do
     setup do
       @project = Factory.create(:project)
       @person = Factory.create(:person)
-      @project.project_memberships.create(:person => @person, :admin => true, :author => true)
-      @ability = Ability.new(@person)
     end
     
-    should_have_project_abilities(%w{read create update destroy})
-    should_have_content_abilities(%w{read create update destroy})
+    should "have appropriate permissions" do
+      @ability = Ability.new(@person)
+      assert_has_project_abilities(%w{create})
+      assert_has_content_abilities([])
+    end
+    
+    context "of a public project" do
+      setup do
+        @project.public_visibility = "visible"
+        assert @project.save
+      end
+      
+      should "be able to read the project" do
+        @ability = Ability.new(@person)
+        assert_has_project_abilities(%w{create read})
+        assert_has_content_abilities(%w{read})
+      end
+    end
+    
+    context "of a copy-templates project" do
+      setup do
+        @project.public_visibility = "copy_templates"
+        assert @project.save
+      end
+      
+      should "be able to copy templates from the project" do
+        @ability = Ability.new(@person)
+        assert_has_project_abilities(%w{create})
+        assert_has_content_abilities([])
+        assert @ability.can?(:copy_templates, @project)
+      end
+    end
   end
+    
 end
