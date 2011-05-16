309a09c25b23c9deabea41da2053d9d1725573c4
Nat Budin <natbudin@gmail.com>
2011-05-16 15:50:31 -0400

    No more ae_users db

--- config/initializers/heroku_external_db.rb
+++ config/initializers/heroku_external_db.rb
@@ -1,6 +1,3 @@
 if ENV['EXTERNAL_DATABASE_URL']
-  HerokuExternalDb.setup_configuration!("USERS", "users")
-  
-  # This has to come last so that it will be the default ActiveRecord configuration.
   HerokuExternalDb.setup_rails_env!
 end
