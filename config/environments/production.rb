e5d3aa024749108d484404d4ea9056edceab6102
Nat Budin <natbudin@gmail.com>
2012-01-30 16:54:32 -0500

    Add asset_sync

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,9 @@ Vellum::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = Proc.new do |source, request|
+    request.ssl? ? "https://#{ENV['AWS_BUCKET']}.s3.amazonaws.com" : "http://#{ENV['AWS_BUCKET']}.s3.amazonaws.com"
+  end
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
