d659b9da628c5b04c4e411a7ee6c7be7ba8d35c7
Nat Budin <natbudin@gmail.com>
2012-02-18 21:54:06 -0500

    Add Sendgrid SMTP settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,3 +56,12 @@ Vellum::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 end
+
+ActionMailer::Base.smtp_settings = {
+  :address        => "smtp.sendgrid.net",
+  :port           => "25",
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => ENV['SENDGRID_DOMAIN']
+}
