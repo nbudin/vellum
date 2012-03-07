81abf8be30459b3ddb7e053ad19229de066455a9
Nat Budin <natbudin@gmail.com>
2012-03-07 12:34:36 -0500

    Log more about invites

--- app/models/project_membership.rb
+++ app/models/project_membership.rb
@@ -14,14 +14,14 @@ class ProjectMembership < ActiveRecord::Base
       return
     end
     
-    logger.debug "Trying to find person with email #{email}"
+    logger.info "Trying to find person with email #{email}"
     self.person = Person.find_by_email(email)
     if email and self.person.nil?
-      logger.debug "Not found, trying Illyan invite"
+      logger.info "Not found, trying Illyan invite"
       begin
         invitee = IllyanClient::Person.new(:person => { :email => email })
         invitee.save
-        logger.debug "Invite successful!  Got back #{invitee.inspect}"
+        logger.info "Invite successful!  Got back #{invitee.inspect}"
         
         invitee = invitee.person
         self.person = Person.create(:email => email, :username => email, :firstname => invitee.firstname, 
