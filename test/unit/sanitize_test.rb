d6a3f096b3146dfacf07a15eeee84951e4e8ebd7
Nat Budin <nbudin@patientslikeme.com>
2012-10-03 11:44:22 -0400

    Follow Postel's law

--- test/unit/sanitize_test.rb
+++ test/unit/sanitize_test.rb
@@ -12,7 +12,7 @@ class SanitizeTest < ActiveSupport::TestCase
   end
 
   test "allow tables" do
-    assert_sanitized("<table><tr><td>A table!</td></tr></table>", "<table><tr><td>A table!</td></tr></table>")
+    assert_sanitized("<table><tr><td>A table!</td></tr></table>", %r{<table>\s*<tr>\s*<td>A table!</td>\s*</tr>\s*</table>})
   end
   
   test "remove non-Vellum classes" do
