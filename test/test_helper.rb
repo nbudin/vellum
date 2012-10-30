f8f6d91aa80fb8ffb05874cadcb0c73350b73766
Nat Budin <nbudin@patientslikeme.com>
2012-10-29 20:06:40 -0400

    Fix some messy requires

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,6 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../config/environment", __FILE__)
 require 'rails/test_help'
-require 'capybara/rails'
 
 class ActiveSupport::TestCase
   # Transactional fixtures accelerate your tests by wrapping each test method
