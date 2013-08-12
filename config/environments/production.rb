ec4b902ee1f46b7f3f0afbcf6fb386180b528f75
Nat Budin <nbudin@patientslikeme.com>
2013-08-12 10:02:15 -0400

    Add missing assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ Vellum::Application.configure do
   config.assets.digest = true
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( document.css wysihtml5-iframe.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
