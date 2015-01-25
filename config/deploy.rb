00449cb32c8650010be2909454bc82ee7515f846
Nat Budin <nbudin@patientslikeme.com>
2015-01-25 14:35:47 -0500

    Don't spit out lots of scary warnings that don't mean anything bad'

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,7 @@ set :deploy_to, '/var/www/vellum'
 # set :format, :pretty
 
 # Default value for :log_level is :debug
-# set :log_level, :debug
+set :log_level, :info
 
 # Default value for :pty is false
 # set :pty, true
