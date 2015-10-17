6dc66247fad492ff8a8df01e77f5c68011cf77b9
Nat Budin <nbudin@patientslikeme.com>
2015-10-17 14:50:43 -0400

    Refactor Ability a little

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,40 +1,63 @@
 class Ability
   include CanCan::Ability
   
+  attr_reader :person
+  
+  PROJECT_CONTENTS = [Doc, Doc::Version, DocTemplate, CsvExport, Map, PublicationTemplate, RelationshipType, Relationship]
+  MAP_CONTENTS = [MappedDocTemplate, MappedRelationshipType]
+  
   def initialize(person)
-    return if person.nil?
+    @person = person
+    return unless person
 
     can :create, Project
-    project_contents = [Doc, DocTemplate, CsvExport, Map, PublicationTemplate, RelationshipType, Relationship]
-    map_contents = [MappedDocTemplate, MappedRelationshipType]
     
     if person.admin?
-      can [:read, :update, :destroy, :change_permissions, :copy_templates], Project
-      can :manage, project_contents
-      can :manage, map_contents
-      can :manage, SiteSettings
+      grant_admin_permissions
     else
-      memberships = person.project_memberships
-      read_project_ids = memberships.map(&:project_id)
-      author_project_ids = memberships.select(&:author?).map(&:project_id)
-      admin_project_ids = memberships.select(&:admin?).map(&:project_id)
-      
-      can([:read, :copy_templates], Project, :id => read_project_ids)
-      can(:read, Project, :public_visibility => "visible")
-      can(:copy_templates, Project, :public_visibility => "copy_templates")
-      can(:update, Project, :id => author_project_ids)
-      can([:destroy, :change_permissions], Project, :id => admin_project_ids)
-      
-      can(:read, project_contents, :project_id => read_project_ids)
-      can(:read, project_contents, :project => {:public_visibility => "visible"})
-      can(:manage, project_contents, :project_id => author_project_ids)
-      
-      can([:test, :publish], PublicationTemplate, :project_id => read_project_ids)
-      can([:test, :publish], PublicationTemplate, :project => {:public_visibility => "visible"})
-      
-      can(:read, map_contents, :map => { :project_id => read_project_ids })
-      can(:read, map_contents, :map => { :project => {:public_visibility => "visible"} })
-      can(:manage, map_contents, :map => { :project_id => author_project_ids })
+      grant_non_admin_permissions
     end
+  end
+  
+  private
+  
+  def grant_admin_permissions
+    can [:read, :update, :destroy, :change_permissions, :copy_templates], Project
+    can :manage, PROJECT_CONTENTS + MAP_CONTENTS + [SiteSettings]
+  end
+  
+  def grant_non_admin_permissions
+    can([:read, :copy_templates], Project, :id => read_project_ids)
+    can(:read, Project, :public_visibility => "visible")
+    can(:copy_templates, Project, :public_visibility => "copy_templates")
+    can(:update, Project, :id => author_project_ids)
+    can([:destroy, :change_permissions], Project, :id => admin_project_ids)
+    
+    can(:read, PROJECT_CONTENTS, :project_id => read_project_ids)
+    can(:read, PROJECT_CONTENTS, :project => {:public_visibility => "visible"})
+    can(:manage, PROJECT_CONTENTS, :project_id => author_project_ids)
+    
+    can([:test, :publish], PublicationTemplate, :project_id => read_project_ids)
+    can([:test, :publish], PublicationTemplate, :project => {:public_visibility => "visible"})
+    
+    can(:read, MAP_CONTENTS, :map => { :project_id => read_project_ids })
+    can(:read, MAP_CONTENTS, :map => { :project => {:public_visibility => "visible"} })
+    can(:manage, MAP_CONTENTS, :map => { :project_id => author_project_ids })
+  end
+  
+  def memberships
+    @memberships ||= person.project_memberships
+  end
+  
+  def read_project_ids
+    @read_project_ids ||= memberships.map(&:project_id)
+  end
+  
+  def author_project_ids
+    @author_project_ids ||= memberships.select(&:author?).map(&:project_id)
+  end
+  
+  def admin_project_ids
+    @admin_project_ids ||= memberships.select(&:admin?).map(&:project_id)
   end
 end
