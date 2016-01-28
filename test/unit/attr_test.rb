55e8a90c034bcae9b5e8e7da8972ce3d7a0492e0
Nat Budin <nbudin@patientslikeme.com>
2016-01-28 08:26:21 -0500

    Replace old requires with new-style ones

--- test/unit/attr_test.rb
+++ test/unit/attr_test.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../test_helper'
+require 'test_helper'
 
 class AttrTest < ActiveSupport::TestCase
   describe "with an existing Attr" do
@@ -9,7 +9,7 @@ class AttrTest < ActiveSupport::TestCase
     end
 
     subject { @attr }
-    
+
     it "should allow the proper values for names" do
       ["a name", "rather-usual", "10 lbs"].each do |value|
         must allow_value(value).for(:name)
