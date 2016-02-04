f0b84f8d64b1a268e80d2966beb95148a098e405
Nat Budin <nbudin@patientslikeme.com>
2016-02-03 21:09:00 -0500

    Use strong parameter for publication templates

--- app/controllers/publication_templates_controller.rb
+++ app/controllers/publication_templates_controller.rb
@@ -3,11 +3,11 @@ require 'format_conversions'
 class PublicationTemplatesController < ApplicationController
   load_and_authorize_resource :project
   load_and_authorize_resource :through => :project
-  
+
   def index
     @publication_templates_by_type = @publication_templates.sort_by(&:name).group_by(&:template_type)
   end
-  
+
   # GET /docs/1
   # GET /docs/1.xml
   def show
@@ -17,11 +17,11 @@ class PublicationTemplatesController < ApplicationController
       format.json { render :json => @publication_template.to_json }
     end
   end
-  
+
   def new
     @doc_templates = @project.doc_templates.all.sort_by { |t| t.name.sort_normalize }
   end
-  
+
   # GET /publication_templates/1;edit
   def edit
     @doc_templates = @project.doc_templates.all.sort_by { |t| t.name.sort_normalize }
@@ -40,44 +40,44 @@ class PublicationTemplatesController < ApplicationController
       end
     end
   end
-  
+
   def test
     @publication_template = @project.publication_templates.find(params[:id])
-    
+
     context_options = { :project => @project }
     if params[:context] && params[:context][:doc_id]
       @doc = @project.docs.find(params[:context][:doc_id])
       context_options[:doc] = @doc
     end
-    
+
     begin
       @output = @publication_template.execute(context_options)
     rescue Exception => e
       @error = e
     end
-    
+
     if params[:raw_preview]
       render :text => @output
     else
       @raw_preview_url = url_for(params.update(:raw_preview => true))
     end
   end
-  
+
   def publish
     @publication_template = @project.publication_templates.find(params[:id])
-    
+
     @attachment = (@publication_template.output_format.to_s != 'html')
-    
-    begin      
+
+    begin
       if @publication_template.doc_template
         @tempfile = Tempfile.new("vellum-publication-#{Time.now}.zip")
 
         @filename = @project.name
         @filename << " - #{@publication_template.doc_template.plural_name}" if @publication_template.doc_template
         @filename << ".zip"
-        
+
         @filetype = "application/zip"
-    
+
         Zip::OutputStream.open(@tempfile.path) do |zipfile|
           @publication_template.doc_template.docs.each do |doc|
             filename = doc.name.gsub(/[\/\\]/, "_")
@@ -89,14 +89,14 @@ class PublicationTemplatesController < ApplicationController
       else
         @tempfile = Tempfile.new("vellum-publication-#{Time.now}.#{@publication_template.output_format}")
         @tempfile.write @publication_template.execute(:project => @project)
-        
+
         @filetype = FormatConversions.mime_type_for(@publication_template.output_format)
         extension = FormatConversions.filename_extension_for(@publication_template.output_format)
         @filename = "#{@project.name} - #{@publication_template.name}#{extension}"
       end
-      
+
       @tempfile.flush
-      
+
       send_file @tempfile.path, :type => @filetype, :disposition => (@attachment ? 'attachment' : 'inline'),
         :filename => @filename
     rescue Exception => e
@@ -137,10 +137,14 @@ class PublicationTemplatesController < ApplicationController
       format.json { head :ok }
     end
   end
-  
+
   private
   def layouts
     @publication_template.project.publication_templates.where(template_type: "layout").order(:name).reject { |t| t == @publication_template }
   end
   helper_method :layouts
+
+  def publication_template_params
+    params.require(:publication_template).permit(:name, :format, :content, :doc_template_id, :layout_id, :template_type)
+  end
 end
